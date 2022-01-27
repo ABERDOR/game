212d3266241cddd3579089ce159e7c031686caea
ABERDOR <97675848+ABERDOR@users.noreply.github.com>
2022-01-27 22:53:48 +0800

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,19 @@
+//alert('123')
+const request= new XMLHttpRequest();
+request.open('GET','../json/js.json',true);
+request.responseType = 'json';
+request.send();
+
+
+request.onload = function() {
+	var name=document.getelementById('name');
+	name.innerHTML='123';
+  var superHeroes = request.response;
+  showname(superHeroes);
+  showlevel(superHeroes);
+}
+function showname(name){
+	var name=document.getelementById('name');
+	name.innerHTML='123';
+
+}
