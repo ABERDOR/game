c7a33328b38914d71db58d6d7129a56825b96cf5
ABERDOR <97675848+ABERDOR@users.noreply.github.com>
2022-01-28 20:54:12 +0800

    Update js.js

--- js/js.js
+++ js/js.js
@@ -4,11 +4,11 @@ url = 'json/js.json';
 
 fetch(url, {})
 .then((response) => {
-  console.log(response);
+  //console.log(response);
   return response.json(); 
 }).then((jsonData) => {
-  showname(jsonData);
-  console.log(jsonData);
+  showname(jsonData['魔藝師']);
+  //console.log(jsonData);
 }).catch((err) => {
   console.log('錯誤:', err);
 })
@@ -28,13 +28,12 @@ request.onload = function() {
 	//showlevel(superHeroes);
 }
 */
+var allch = ['cho','ch1','ch2','ch3'];
+var a=0;
 function showname(obj){
-	//console.log(obj);
-	var name=document.getElementById('name');
-	var name1=document.getElementById('name1');
-	var name2=document.getElementById('name2');
-	name.innerHTML='13';
-	name1.innerHTML=obj.san;
-	name2.innerHTML=obj['數值']['san'];
-	
+	if(a<4){
+	var ch=document.getElementById(allch[a]);
+	a+1;
+	ch.innerHTML+='<div>'+obj['職業']+'</div>';
+	}
 }
