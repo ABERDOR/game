5f7b29bae583bd1f178f0c8a9b5062e4a849f610
ABERDOR <97675848+ABERDOR@users.noreply.github.com>
2022-01-28 23:44:30 +0800

    Update js.js

--- js/js.js
+++ js/js.js
@@ -7,7 +7,7 @@ fetch(url, {})
   //console.log(response);
   return response.json(); 
 }).then((jsonData) => {
-  showname(jsonData['魔藝師']);
+  show(jsonData['魔藝師']);
   //console.log(jsonData);
 }).catch((err) => {
   console.log('錯誤:', err);
@@ -28,24 +28,47 @@ request.onload = function() {
 	//showlevel(superHeroes);
 }
 */
+var add = '<button onclick=\"addf()\">';
+var reduce = '<button onclick>=\"reducef()\"';
 var allch = ['ch0','ch1','ch2','ch3'];
 var a=0;
-function showname(obj){
+function addf(){
+	
+
+
+}
+function reducef(){
+
+
+}
+function show(obj){
 	if(a<4){
 	var ch=document.getElementById(allch[a]);
 	a+1;
 	ch.innerHTML+='<div>'+obj['職業']+'</div>';
 	for (skSP in obj['skillSP']){
 		//console.log(skSP);
-		ch.innerHTML+='<div title=\"'+obj['skillSP'][skSP]+'\">'+skSP+"</div>";
+		ch.innerHTML+='<div title=\"'+obj['skillSP'][skSP]+'\">'
+			+skSP
+			+"</div>";
 	}
 	for (va in obj['value']){
 		//console.log(va);
-		ch.innerHTML+='<div>'+va+obj['value'][va]+'</div>';
+		ch.innerHTML+='<div>'
+			+va
+			+<a>
+			+obj['value'][va]
+			+</a>
+			+'</div>';
 	}
 	for (sk in obj['skill']){
 		//console.log(sk);
-		ch.innerHTML+='<div title=\"'+obj['skillEX'][sk]+'\">'+sk+obj['skill'][sk]+'</div>';
+		ch.innerHTML+='<div title=\"'+obj['skillEX'][sk]+'\">'
+			+sk
+			+<a>
+			+obj['skill'][sk]
+			+</a>
+			+'</div>';
 	}
 		
 	//ch.innerHTML+='<div>'+obj['職業']+'</div>';	
